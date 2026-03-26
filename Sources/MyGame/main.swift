77ff1feb2f5beabbf251545dcf3c735440d20777
k0pernicus <dev@carette.xyz>
2026-03-26 16:50:13 +0100

    Integration of ImGui for macOS

--- Sources/MyGame/main.swift
+++ Sources/MyGame/main.swift
@@ -1,26 +1,35 @@
 import CRaylib
+import ImGui
 
-let screenWidth: Int32 = 800
-let screenHeight: Int32 = 600
+let screenWidth: Int32 = 1200
+let screenHeight: Int32 = 800
 
-#if os(WASI)
-    InitWindow(screenWidth, screenHeight, "WASM C Raylib from Swift!")
-#else
-    InitWindow(screenWidth, screenHeight, "Raw C Raylib from Swift!")
-#endif
+InitWindow(screenWidth, screenHeight, "Swift + Raylib + ImGui")
 SetTargetFPS(60)
 
 let rayWhite = Color(r: 245, g: 245, b: 245, a: 255)
 let darkGray = Color(r: 80, g: 80, b: 80, a: 255)
 
-while !WindowShouldClose() {
+rlImGuiSetup(true)
 
+var showDemoWindow = true
+
+while !WindowShouldClose() {
     BeginDrawing()
 
     ClearBackground(rayWhite)
     DrawText("It's alive... ALIVE!", 300, 300, 20, darkGray)
 
+    rlImGuiBegin()
+
+    if showDemoWindow {
+        ImGui.ShowDemoWindow(&showDemoWindow)
+    }
+
+    rlImGuiEnd()
+
     EndDrawing()
 }
 
+rlImGuiShutdown()
 CloseWindow()
