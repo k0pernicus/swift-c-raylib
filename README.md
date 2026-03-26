7231a159b877e2ff1bf82e6bc91702ac22b9ee59
k0pernicus <dev@carette.xyz>
2026-03-26 17:03:09 +0100

    Fixed the WASM build

--- README.md
+++ README.md
@@ -13,3 +13,9 @@ Make sure you installed:
 To run on macOS: `make run-mac`
 
 To run using WASI: `make run-wasm`
+
+## Update (26th of March)
+
+Based on some cricisms over my blog post I included Dear ImGui as a dependency of this project to demonstrate how easy it is to use a C++ codebase in a Swift project.
+
+The ImGui integration is currently **not** in the WASM build.
--- Sources/MyGame/main.swift
+++ Sources/MyGame/main.swift
@@ -1,35 +1,54 @@
 import CRaylib
-import ImGui
+
+#if os(macOS)
+    import ImGui
+#endif
 
 let screenWidth: Int32 = 1200
 let screenHeight: Int32 = 800
 
-InitWindow(screenWidth, screenHeight, "Swift + Raylib + ImGui")
+#if os(macOS)
+    InitWindow(screenWidth, screenHeight, "Swift + Raylib + ImGui")
+#else
+    InitWindow(screenWidth, screenHeight, "Swift + Raylib + WASM")
+#endif
 SetTargetFPS(60)
 
 let rayWhite = Color(r: 245, g: 245, b: 245, a: 255)
 let darkGray = Color(r: 80, g: 80, b: 80, a: 255)
 
-rlImGuiSetup(true)
+#if os(WASI)
 
-var showDemoWindow = true
+    while !WindowShouldClose() {
+        BeginDrawing()
+        ClearBackground(rayWhite)
+        DrawText("It's alive... ALIVE!", 300, 300, 20, darkGray)
+        EndDrawing()
+    }
+    CloseWindow()
 
-while !WindowShouldClose() {
-    BeginDrawing()
+#else
 
-    ClearBackground(rayWhite)
-    DrawText("It's alive... ALIVE!", 300, 300, 20, darkGray)
+    rlImGuiSetup(true)
+    var showDemoWindow = true
+    while !WindowShouldClose() {
+        BeginDrawing()
 
-    rlImGuiBegin()
+        ClearBackground(rayWhite)
+        DrawText("It's alive... ALIVE!", 300, 300, 20, darkGray)
 
-    if showDemoWindow {
-        ImGui.ShowDemoWindow(&showDemoWindow)
+        rlImGuiBegin()
+
+        if showDemoWindow {
+            ImGui.ShowDemoWindow(&showDemoWindow)
+        }
+
+        rlImGuiEnd()
+
+        EndDrawing()
     }
 
-    rlImGuiEnd()
+    rlImGuiShutdown()
+    CloseWindow()
 
-    EndDrawing()
-}
-
-rlImGuiShutdown()
-CloseWindow()
+#endif
